018bdcb3aed6ef9d0a88ba3be8bbfefc0c4023bc
Jack Hopkins <jack.hopkins@clickwork.app>
2022-03-31 14:30:51 +0100

    Added basic setup, will continue later

---
+++ lib/screens/menus/scenario_screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/widgets.dart';
+
+class ScenarioPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+}
